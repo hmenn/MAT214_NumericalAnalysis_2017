89ad915e0aed52b7a13c28fdedeace694a4cad8e
Hasan MEN <hmen.56@gmail.com>
2017-03-31 23:46:41 +0300

    [HW2] gaussian elimination added

--- HW2/131044009_HW2.c
+++ HW2/131044009_HW2.c
@@ -14,27 +14,11 @@ void printUsage(const char *execname){
 matrix_t* readMatrix(const char *filename);
 void printMatrix(const matrix_t* matrix);
 void solveGESP(matrix_t* matrix);
+void freeMatrix(matrix_t* matrix);
+void swapRows(matrix_t* matrix,int r1,int r2);
+void reducePivots(matrix_t* matrix,int i);
+double* getXVals(const matrix_t* matrix);
 
-void printMatrix(const matrix_t* matrix){
-
-  int i,j;
-  int row,column;
-
-  row = matrix->row;
-  column = matrix->column;
-
-  printf("-------------------------------------------\n");
-  printf("                  Matrix\n");
-  printf("-------------------------------------------\n");
-  for(i=0;i<row;++i){
-    for(j=0;j<column;++j){
-      printf("%08.4f ",matrix->array[i][j]);
-      if(j==column-2)
-        printf("| " );
-    }
-    printf("\n");
-  }
-}
 
 int main(int argc,char *argv[]){
   matrix_t *matrix=NULL;
@@ -49,15 +33,129 @@ int main(int argc,char *argv[]){
   }
 
   printMatrix(matrix);
-  
+  solveGESP(matrix);
+  double *results = getXVals(matrix);
+  //freeMatrix(matrix);
+
   return 0;
+}
+
+void freeMatrix(matrix_t* matrix){
+  int i=0;
+  for(i=0;i<matrix->row;++i)
+    free(matrix->array[i]);
+}
+
+
+double* getXVals(const matrix_t* matrix){
+  int column = matrix->column;
+  int row = matrix->row;
+  double * array = malloc(sizeof(double)*column);
+
+  printf("%f - %f \n", matrix->array[row-1][column-1],matrix->array[row-1][column-2]);
+  array[column -1 ] = matrix->array[row-1][column-1]/matrix->array[row-1][column-2];
+  printf("X[%d]:%f\n",column-1,array[column-1]);
+
+  return array;
+}
+
+void swapRows(matrix_t* matrix,int r1,int r2){
+  for(int i=0;i<matrix->column;++i){
+    double temp = matrix->array[r1][i];
+    matrix->array[r1][i]=matrix->array[r2][i];
+    matrix->array[r2][i]=temp;
+  }
+}
+
+
+
+void solveGESP(matrix_t* matrix){
+  int i=0,j=0,k=0;
+  int row = matrix->row;
+  int column = matrix->column;
+  double *s  = (double *)malloc(sizeof(double)*(column-1));
+
+  printf("Row: %d, Column:%d\n",row,column);
+
+  /* finding scaled partial pivots */
+  for(i=0;i<row;++i){
+    s[i] = fabs(matrix->array[i][0]);
+    for(j=0;j<column-1;++j){
+      if(fabs(matrix->array[i][j]) > s[i])
+        s[i] = fabs(matrix->array[i][j]);
+    }
+  }
+
+  for(i=0;i<column-1;++i)
+    printf("S[%d] = %f | ",i+1,s[i]);
+  printf("\n");
+
+  for(i=0;i<row-1;++i){
+    for(j=i;j<column;++j){
+      int maxPivotIndex = i;
+      double maxPivot = fabs(matrix->array[i][j]) / s[i];
+      //printf("MaxPivot:%f - ",maxPivot);
+
+      for(k=i+1;k<row;++k){
+        double second = fabs(matrix->array[k][j]) / s[k];
+        //printf("Second:%f\n",second );
+        if(second>maxPivot){
+          maxPivot=second;
+          maxPivotIndex=k;
+        }
+      }
+
+      if(j==i){
+        printf("Max Pivot Index: %d, Pivot:%f\n",maxPivotIndex+1,maxPivot);
+        printf("Change Row:%d with Row:%d\n",i+1,maxPivotIndex+1);
+        swapRows(matrix,i,maxPivotIndex);
+        reducePivots(matrix,i); // i nin alt sutununu sıfırla
+        printMatrix(matrix);
+      }
+    }
+  }
+
+}
+
+void reducePivots(matrix_t* matrix,int index){
+
+  int i,j;
+  for(i=index+1;i<matrix->row;++i){
+    double coef = matrix->array[i][index] / matrix->array[index][index];
+    printf("Coeff:%f\n",coef);
+    for(j=0;j<matrix->column;++j){
+      matrix->array[i][j] = matrix->array[i][j] - coef * matrix->array[index][j];
+    }
+  }
+
+}
+
+void printMatrix(const matrix_t* matrix){
+
+  int i,j;
+  int row,column;
+
+  row = matrix->row;
+  column = matrix->column;
+
+  printf("-------------------------------------------\n");
+  printf("                  Matrix\n");
+  printf("-------------------------------------------\n");
+  for(i=0;i<row;++i){
+    for(j=0;j<column;++j){
+      printf("%08.3f ",matrix->array[i][j]);
+      if(j==column-2)
+        printf("| " );
+    }
+    printf("\n");
+  }
 }
 
 matrix_t* readMatrix(const char *filename){
   FILE *fp=NULL;
   matrix_t *matrix=NULL;
   int row=0,column=1,i=0,j=0;
-  int junk1;
+  double junk1;
   char junk2;
 
   if( (fp = fopen(filename,"r"))==NULL){
@@ -65,13 +163,13 @@ matrix_t* readMatrix(const char *filename){
     return NULL;
   }
 
-  while(fscanf(fp,"%d%c",&junk1,&junk2) && junk2!='\n'){
+  while(fscanf(fp,"%lf%c",&junk1,&junk2) && junk2!='\n'){
     ++column;
   }
   rewind(fp);
 
   matrix = (matrix_t*)malloc(sizeof(matrix_t));
-  matrix->array = (double **)malloc(sizeof(double **)*(column-1));
+  matrix->array = (double **)malloc(sizeof(double *)*(column-1));
   for(i=0;i<column;++i)
     matrix->array[i] = (double*)malloc(sizeof(double)*column);
 
